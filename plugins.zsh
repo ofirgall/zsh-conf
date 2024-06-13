603515d7d2f41f0238a0eff765fc301c777a2aed
Ofir Gal <ofirgal753@gmail.com>
2024-06-13 20:34:44 +0300

    fixed cargo and rust completions

--- plugins.zsh
+++ plugins.zsh
@@ -2,6 +2,11 @@
 #		  Plugins
 # ---------------------------
 ## oh-my-zsh plugins ##
+
+# Add oh-my-zsh completions dir
+fpath+=$ZSH_CACHE_DIR/completions/
+mkdir $ZSH_CACHE_DIR/completions/
+
 zinit ice wait lucid
 zinit snippet OMZP::pip
 
