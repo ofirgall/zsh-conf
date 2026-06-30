cfdc2b0358eee0e91f9d9f4243f4942c0436be0f
Ofir Gal <ofirgal753@gmail.com>
2026-06-30 14:16:34 +0300

    bind to edit cmd-line

--- binds.zsh
+++ binds.zsh
@@ -1,1 +1,6 @@
 bindkey "^ " autosuggest-accept # Accept suggestions with ctrl+space
+
+# Edit cmd-line in editor with ctrl+x+e
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^X^E" edit-command-line
